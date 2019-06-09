5561d519231de156b6395ddbf0d632b3581caa39
Bessie <33269462+BessieChen@users.noreply.github.com>
2019-06-09 21:59:34 +0800

    Create Solution.java

---
+++ 20. Valid Parentheses/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        boolean valid = true;
+        Stack<Character> stack_of_char = new Stack<>();
+        for(int i = 0; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if(c == '(' || c == '[' || c == '{')
+            {
+                stack_of_char.push(c);
+            }
+            else
+            {
+                if(stack_of_char.empty())
+                {
+                    valid = false;
+                }
+                else if(stack_of_char.peek()=='(' && c == ')'
+                        || stack_of_char.peek() == '[' && c == ']'
+                        || stack_of_char.peek() == '{' && c == '}')
+                {
+                    stack_of_char.pop();
+                }
+                else {
+                    valid = false;
+                }
+            }
+        }
+        if(!stack_of_char.empty())
+        {
+            valid = false;
+        }
+        return valid;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println((new Solution()).isValid("()[]{}"));
+        System.out.println((new Solution()).isValid("([]{}"));
+    }
+}
+//不能直接push，应该先判断是左括号还是右括号!
