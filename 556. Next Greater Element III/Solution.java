6c7795ccdf9bc4eb59e221ee7b6ad44f702da1e1
Bessie <33269462+BessieChen@users.noreply.github.com>
2019-08-01 05:44:41 +0800

    Create Solution.java

---
+++ 556. Next Greater Element III/Solution.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int nextGreaterElement(int n) {
+
+        char[] arr = String.valueOf(n).toCharArray();
+        int bFirst = arr.length-1;
+        while(bFirst >= 1 && arr[bFirst] <= arr[bFirst-1])//找B部分：非递增部分，例如44321.
+        {
+            bFirst--;
+        }
+        
+        if(bFirst == 0) return -1;
+        
+        int aLast = bFirst - 1;
+        
+        //现在找B部分中比aLast所对应的值a稍微大一点的值b：因为B部分是降序的，最小值就是最后一个，然后往前找。
+        for(int i = arr.length-1; i >= bFirst; i--)
+        {
+            if(arr[i] > arr[aLast])
+            {
+                swap(arr, aLast, i);
+                reverse(arr, bFirst, arr.length-1);
+                break;
+            }
+        }
+          
+        
+        try 
+        {
+            return Integer.valueOf(String.valueOf(arr));
+        } 
+        catch(NumberFormatException e)//题目的意思是如果超出int范围的话也是返回-1
+        {
+            return -1;
+        }
+        
+    }
+    
+    private static void swap(char[] arr, int index1, int index2)
+    {
+        char temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    
+    private static void reverse(char[] arr, int startIndex, int endIndex)
+    {
+        while(startIndex < endIndex)
+        {
+            swap(arr, startIndex++, endIndex--);
+        }
+    }
+}
