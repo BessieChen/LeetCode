3fee1a5e6b2895b9a79544bedac781be16c4d30d
Bessie <33269462+BessieChen@users.noreply.github.com>
2019-08-14 00:36:02 +0800

    Create Solution2.java

---
+++ 682. Baseball Game/Solution2.java
@@ -0,0 +1,147 @@
+class Solution2 {
+    public int calPoints(String[] ops) {
+        return calPoints1(ops);
+    }
+    
+    /*
+    method 2:
+    Runtime: 1 ms, faster than 100.00% of Java online submissions for Baseball Game.
+    Memory Usage: 36 MB, less than 100.00% of Java online submissions for Baseball Game.
+    */
+    private int calPoints2(String[] ops) {
+        int[] arr = new int[ops.length];
+        int size = 0;
+        int sum = 0;
+        
+        for(String s : ops)
+        {
+            switch(s)
+            {
+                case "C":
+                    {
+                        sum -= arr[--size];
+                        break;
+                    }
+                case "D":
+                    {
+                        arr[size] = arr[size-1]*2;
+                        sum += arr[size];
+                        size++;
+                        break;
+                    }
+                case "+":
+                    {
+                        arr[size] = arr[size-1]+arr[size-2];
+                        sum += arr[size];
+                        size++;
+                        break;
+                    }
+                default:
+                {
+                    arr[size] = Integer.valueOf(s);
+                    sum += arr[size];
+                    size++;
+                    break;
+                }
+            }
+        }
+        return sum;
+    }
+    
+    
+    /*
+    method 1:
+    Runtime: 1 ms, faster than 100.00% of Java online submissions for Baseball Game.
+    Memory Usage: 35.8 MB, less than 100.00% of Java online submissions for Baseball Game.
+    */
+    private int calPoints1(String[] ops) {
+        int[] arr = new int[ops.length];
+        int[] sum = new int[ops.length];
+        int size = 0;
+        
+        for(String s : ops)
+        {
+            if(s.equals("C"))
+            {
+                if(size>0)
+                {
+                    size--;
+                }
+            }
+            else if(s.equals("D"))
+            {
+                if(size > 0)
+                {
+                    arr[size] = arr[size-1]*2;
+                    sum[size] = sum[size-1]+arr[size];
+                    size++;
+                }
+            }
+            else if(s.equals("+"))
+            {
+                if(size > 1)
+                {
+                    arr[size] = arr[size-1]+arr[size-2];
+                    sum[size] = sum[size-1]+arr[size];
+                    size++; 
+                }
+            }
+            else
+            {
+                arr[size] = Integer.valueOf(s);
+                sum[size] = size == 0 ? arr[size] : sum[size-1]+arr[size];
+                size++;
+            }
+        }
+        return sum[size-1];
+    }
+    
+    //wrong:
+    /*private int calPoints(String[] ops) {
+        int[] arr = new int[ops.length];//no:ops.length()
+        int[] sum = new int[ops.length];
+        int size = 0;
+        
+        for(String s : ops)
+        {
+            char c = s.charAt(0);
+            if(Character.isLetter(c))
+            {
+                if(c == 'C')
+                {
+                    if(size > 0)
+                    {
+                        size--;
+                    } 
+                }
+                else
+                {
+                    if(size > 0)
+                    {
+                        arr[size] = arr[size-1]*2;
+                        sum[size] = sum[size-1] + arr[size];
+                        size++;
+                    }
+                }
+            }
+            else if(Character.isDigit(c))
+            {
+                arr[size] = Integer.valueOf(s);//no:s.toDigit(); 
+                if(size == 0) sum[size] = arr[size];
+                else sum[size] = sum[size-1]+arr[size];//note!
+                size++;
+            }
+            else
+            {
+                if(size > 1)
+                {
+                    arr[size] = arr[size-1] + arr[size-2];   
+                    sum[size] = sum[size-1]+arr[size];
+                    size++;
+                }
+            }
+        }
+        return size == 0 ? 0 : sum[size-1];
+    }
+    */
+}
