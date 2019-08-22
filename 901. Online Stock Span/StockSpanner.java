a097b2cfe874ecf032d9e028b4eb8e3fc6e68455
Bessie <33269462+BessieChen@users.noreply.github.com>
2019-08-23 07:54:52 +0800

    Update StockSpanner.java

--- 901. Online Stock Span/StockSpanner.java
+++ 901. Online Stock Span/StockSpanner.java
@@ -1,50 +1,71 @@
 
-class StockSpanner2 {
-    Stack<int[]> stack = new Stack<>();
-    public int next(int price) {
-        int res = 1;
-        while (!stack.isEmpty() && stack.peek()[0] <= price)
-            res += stack.pop()[1];
-        stack.push(new int[]{price, res});
-        return res;
-    }
-}
-
-
-class StockSpanner3{
-        Stack<Item> s = new Stack<>();
-
-        public int next(int price) {
-            int lessOrEqual = 1;
-            while (!s.isEmpty() && s.peek().price <= price)
-                lessOrEqual += s.pop().lessOrEqual;
-            s.push(new Item(lessOrEqual, price));
-            return s.peek().lessOrEqual;
-        }
-
-        class Item {
-            final int lessOrEqual, price;
-
-            Item(int lessCount, int val) {
-                this.lessOrEqual = lessCount;
-                this.price = val;
-            }
-        }
-    }
-
-
+/*
+method 1:
+Runtime: 73 ms, faster than 99.64% of Java online submissions for Online Stock Span.
+Memory Usage: 70 MB, less than 10.00% of Java online submissions for Online Stock Span.
+*/
 class StockSpanner {
-    private List<Integer> list = new ArrayList<>();
-    private List<Integer> prev = new ArrayList<>();
 
+    private int[] pr = new int[10001];
+    private int[] fl = new int[10001];
+    private int size = 0;
+    
+    
     public int next(int price) {
-        list.add(price);
-        int i = list.size() - 2;
-        while (!prev.isEmpty() && i >= 0 && list.get(i) <= price) i = prev.get(i);
-        prev.add(i);
-        return list.size() - 1 - i;
+        int ret;
+        if(size == 0)
+        {
+            pr[size] = price;
+            fl[size] = -1;
+            ret = size + 1;
+            //System.out.println("1size: "+(size)+" fl[size]: "+fl[size]);
+            size++;
+            return ret;
+        }
+        
+        pr[size] = price;
+        if(pr[size-1] > price)
+        {
+            fl[size] = size-1;
+            ret = size - fl[size];
+            //System.out.println("2size: "+(size)+" fl[size]: "+fl[size]);
+            size++;
+            return ret;
+        }
+        
+        int jump = fl[size-1];
+        if(jump == -1)
+        {
+            fl[size] = -1;
+            ret = size + 1;
+            //System.out.println("3size: "+(size)+" fl[size]: "+fl[size]);
+            size++;
+            return ret;
+        }
+        
+        while(pr[jump] <= pr[size])
+        {
+            if(fl[jump] == -1)
+            {
+                fl[size] = -1;
+                ret = size + 1;
+                //System.out.println("3size: "+(size)+" fl[size]: "+fl[size]);
+                size++;
+                return ret;
+            }
+           
+            jump = fl[jump];
+        }
+        fl[size] = jump;
+        //System.out.println("4size: "+(size)+" fl[size]: "+fl[size]);
+        ret = size - fl[size];
+        size++;
+        return ret;
     }
 }
+
+
+
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
