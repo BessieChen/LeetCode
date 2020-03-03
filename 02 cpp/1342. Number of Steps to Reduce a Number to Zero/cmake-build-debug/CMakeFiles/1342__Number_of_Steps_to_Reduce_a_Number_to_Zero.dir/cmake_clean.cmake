f519e3c74e51e4caa2ab53d111416e4dd0bb065c
Bessie <bchen372@gatech.edu>
2020-03-03 11:02:51 +0800

    cpp solution to 1342.

---
+++ 02 cpp/1342. Number of Steps to Reduce a Number to Zero/cmake-build-debug/CMakeFiles/1342__Number_of_Steps_to_Reduce_a_Number_to_Zero.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1342__Number_of_Steps_to_Reduce_a_Number_to_Zero"
+  "1342__Number_of_Steps_to_Reduce_a_Number_to_Zero.pdb"
+  "CMakeFiles/1342__Number_of_Steps_to_Reduce_a_Number_to_Zero.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1342__Number_of_Steps_to_Reduce_a_Number_to_Zero.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 02 cpp/1342. Number of Steps to Reduce a Number to Zero/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+class Solution {
+public:
+//    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of Steps to Reduce a Number to Zero.
+//    Memory Usage: 7.4 MB, less than 100.00% of C++ online submissions for Number of Steps to Reduce a Number to Zero.
+    int numberOfSteps (int num) {
+        int ret = 0;
+        while(num > 0)
+        {
+            if(num % 2 == 0)
+                num /= 2;
+            else
+                num -= 1;
+            ret++;
+        }
+        return ret;
+    }
+
+//    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of Steps to Reduce a Number to Zero.
+//    Memory Usage: 7.2 MB, less than 100.00% of C++ online submissions for Number of Steps to Reduce a Number to Zero.
+    int numberOfSteps2 (int num) {
+        if(!num) return 0;
+        int ret = 0;
+        while(num)
+        {
+            ret += num & 1 ? 2 : 1;
+            num >>= 1;
+        }
+        return ret - 1;
+    }
+
+    int numberOfSteps3 (int num) {
+        return std::bitset<32>(num).size() - std::bitset<32>(num).count(); //std::bitset<32>(num).size() == 32
+    }
+
+//    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of Steps to Reduce a Number to Zero.
+//    Memory Usage: 7.6 MB, less than 100.00% of C++ online submissions for Number of Steps to Reduce a Number to Zero.
+    int numberOfSteps4 (int num) {
+        return log2(num) + std::bitset<32>(num).count(); //std::bitset<32>(num).size() == 32
+    }
+};
+
+int main() {
+    int input = 14;
+    std::cout<<"#1: " <<Solution().numberOfSteps(input)<<std::endl;
+    std::cout<<"#2: " <<Solution().numberOfSteps2(input)<<std::endl;
+    std::cout<<"#3: wrong! " <<Solution().numberOfSteps3(input)<<std::endl;
+    std::cout<<"#4: " <<Solution().numberOfSteps4(input)<<std::endl;
+    std::cout<<log2(14)<<std::endl;//3.80735
+    std::cout<<(int)(log2(14))<<std::endl;//3
+    std::cout<<"-----"<<std::endl;
+    std::cout<<log2(16)<<std::endl;//4
+    std::cout<<(int)(log2(16))<<std::endl;//4
+    std::cout<<"-----"<<std::endl;
+    std::cout<<(int)(log2(14))+1<<std::endl;//14: 0b 1110; (int)(log2(14)) == 3,
+    std::cout<<(int)(log2(16))+1<<std::endl;//16: 0b 10000
+    return 0;
+}
