dba608cd41afe844081655a7172a4b65ab06227d
Bessie <bchen372@gatech.edu>
2020-03-04 11:30:39 +0800

    python solution to 1221.

--- 03 python/1221. Split a String in Balanced Strings/Solution.py
+++ 03 python/1221. Split a String in Balanced Strings/Solution.py
@@ -2,11 +2,6 @@ from itertools import accumulate as acc
 class Solution:
     '''Runtime: 32 ms, faster than 29.08% of Python3 online submissions for Split a String in Balanced Strings.
     Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Split a String in Balanced Strings.'''
-    def balancedStringSplit(self, s: str) -> int:
-        return list(acc(-1 if c == 'L' else 1 for c in s)).count(0)
-
-    '''Runtime: 32 ms, faster than 29.08% of Python3 online submissions for Split a String in Balanced Strings.
-    Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Split a String in Balanced Strings.'''
     def balancedStringSplit2(self, s: str) -> int:
         cnt = 0
         ret = 0
@@ -14,6 +9,11 @@ class Solution:
             cnt += 1 if c == 'R' else -1
             ret += 1 if cnt == 0 else 0
         return ret
+
+    '''Runtime: 32 ms, faster than 29.08% of Python3 online submissions for Split a String in Balanced Strings.
+        Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Split a String in Balanced Strings.'''
+    def balancedStringSplit(self, s: str) -> int:
+        return list(acc(-1 if c == 'L' else 1 for c in s)).count(0)
 
     '''Runtime: 28 ms, faster than 68.26% of Python3 online submissions for Split a String in Balanced Strings.
     Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Split a String in Balanced Strings.'''
