c35964485a21cbf646ad81baf439f865b0248e65
Bessie <bchen372@gatech.edu>
2020-03-06 11:00:23 +0800

    cpp solution to 1108.

---
+++ 02 cpp/1108. Defanging an IP Address/cmake-build-debug/CMakeFiles/1108__Defanging_an_IP_Address.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1108__Defanging_an_IP_Address"
+  "1108__Defanging_an_IP_Address.pdb"
+  "CMakeFiles/1108__Defanging_an_IP_Address.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1108__Defanging_an_IP_Address.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 02 cpp/1108. Defanging an IP Address/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution{
+public:
+//    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Defanging an IP Address.
+//    Memory Usage: 7.3 MB, less than 100.00% of C++ online submissions for Defanging an IP Address.
+    string defangIPaddr(string address) {
+        string s = "";
+        for(const char& c : address)
+        {
+            if( c == '.')
+            {
+                s += "[.]";
+            } else{
+                s += c;
+            }
+        }
+        return s;
+    }
+
+//    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Defanging an IP Address.
+//    Memory Usage: 7.3 MB, less than 100.00% of C++ online submissions for Defanging an IP Address.
+    string defangIPaddr2(string address) {
+        for(int i = 0 ; i < address.size(); )
+        {
+            if(address[i] == '.')
+            {
+                address.replace(i, 1, "[.]");
+                i += 2;
+            } else{
+                i++;
+            }
+        }
+        return address;
+    }
+
+//    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Defanging an IP Address.
+//    Memory Usage: 7.6 MB, less than 100.00% of C++ online submissions for Defanging an IP Address.
+    string defangIPaddr3(string address) {
+        for(int i = address.size()-1; i >= 0 ; i --)
+        {
+            if(address.at(i) == '.')
+            {
+                address.replace(i, 1, "[.]");
+            }
+        }
+        return address;
+    }
+
+//    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Defanging an IP Address.
+//    Memory Usage: 7.4 MB, less than 100.00% of C++ online submissions for Defanging an IP Address.
+    string defangIPaddr4(string address) {
+        for(int i = 1; i < address.size(); )
+        {
+            if(address.at(i) == '.')
+            {
+                address = address.substr(0, i) + "[.]" + address.substr(i+1);
+                i += 3;
+            }
+            else i++;
+        }
+        return address;
+    }
+
+//    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Defanging an IP Address.
+//    Memory Usage: 7.7 MB, less than 100.00% of C++ online submissions for Defanging an IP Address.
+    string defangIPaddr5(string address) {
+        for(int i = address.size()-2; i >= 0 ; i--)
+        {
+            if(address.at(i) == '.')
+            {
+                address = address.substr(0, i) + "[.]" + address.substr(i+1);
+            }
+        }
+        return address;
+    }
+};
+
+void testString()
+{
+    string test = "abc";
+    cout<<test.size()<<endl;
+    cout<<test[test.size()]<<endl;
+}
+int main() {
+    testString();
+    string s = "255.100.50.0";
+    cout<<"#1: "<<Solution().defangIPaddr(s)<<endl;
+    cout<<"#2: "<<Solution().defangIPaddr2(s)<<endl;
+    cout<<"#3: "<<Solution().defangIPaddr3(s)<<endl;
+    cout<<"#4: "<<Solution().defangIPaddr4(s)<<endl;
+    cout<<"#5: "<<Solution().defangIPaddr5(s)<<endl;
+    return 0;
+}
