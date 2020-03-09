950377fef87a0f2d66a0d63fe17ad27add85a5be
Bessie <bchen372@gatech.edu>
2020-03-09 22:37:26 +0800

    cpp solution to 1365.

---
+++ 02 cpp/1365. How Many Numbers Are Smaller Than the Current Number/cmake-build-debug/CMakeFiles/1365__How_Many_Numbers_Are_Smaller_Than_the_Current_Number.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1365__How_Many_Numbers_Are_Smaller_Than_the_Current_Number"
+  "1365__How_Many_Numbers_Are_Smaller_Than_the_Current_Number.pdb"
+  "CMakeFiles/1365__How_Many_Numbers_Are_Smaller_Than_the_Current_Number.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1365__How_Many_Numbers_Are_Smaller_Than_the_Current_Number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 02 cpp/1365. How Many Numbers Are Smaller Than the Current Number/cmake-build-debug/CMakeFiles/1365__How_Many_Numbers_Are_Smaller_Than_the_Current_Number.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1365__How_Many_Numbers_Are_Smaller_Than_the_Current_Number.
+# This may be replaced when dependencies are built.
