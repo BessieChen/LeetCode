ccd39796e35a8250e6f279dcbd551a0ac0888ce7
Bessie <bchen372@gatech.edu>
2020-05-25 02:35:33 +0800

    655

--- 02 cpp/665. Non-decreasing Array/main.cpp
+++ 02 cpp/665. Non-decreasing Array/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int cnt = 0;
+        for(int i = 0; i < nums.size()-1; i++)
+        {
+            if(nums[i] <= nums[i+1])
+                continue;
+            else if(cnt == 0)
+            {
+                if(i-1 >= 0)
+                {
+                    if(nums[i-1] > nums[i+1]) nums[i+1] = nums[i]; // case 2
+                    else nums[i] = nums[i+1];     // case 1
+                }
+                else nums[i] = nums[i+1]; //case 3
+
+                cnt ++;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> input = {3,4,2,3};
+    std::cout << Solution().checkPossibility(input)<<endl; //false;
     return 0;
 }
